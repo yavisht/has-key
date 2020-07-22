02bbc65e2a59e559a0e64d386abd3f5c3ebde85a
Yavisht Katgara <yavisht.katgara@gmail.com>
2020-07-23 09:05:10 +1000

    fix(esm): switch back to module exports

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// return true if key exists
-export default function hasKey(obj, key) {
+module.exports = function hasKey(obj, key) {
   if (Object.keys(obj).includes(key)) {
     return true;
   } else {
     return false;
   }
-}
+};
